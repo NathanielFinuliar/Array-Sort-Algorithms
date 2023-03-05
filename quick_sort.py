2e3dcec352dd4a725f21eb4ef5176d312e3c4c3c
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-05 17:09:41 +0800

    Added ordinal_indicator for printing Numbered Passes

--- quick_sort.py
+++ quick_sort.py
@@ -5,3 +5,15 @@ print("\t\t\t","%"*37)
 print(" ")
 
 print("\t  ARRAY VALUES:[81, 96, 77, 67, 43, 99, 65, 32, 62, 60]")
+
+def ordinal_indicator(n):
+    if n == 1:
+        return "1st"
+    elif n == 2:
+        return "2nd"
+    elif n == 3:
+        return "3rd"
+    elif n in [4, 5, 6, 7, 8]:
+        return str(n) + "th"
+    else:
+        return str(n)
