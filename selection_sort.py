9e56c8c6294dbab202a178acd35dea2d1a338851
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-05 15:43:13 +0800

    Prints Assigned Array Value to be sorted

--- selection_sort.py
+++ selection_sort.py
@@ -2,3 +2,5 @@ print("\t\t\t", "%"*37)
 print("\t\t\t $"," "*7, "SELECTION SORTING", " " *7,"$")
 print("\t\t\t $"," " * 2, "CODED BY NATHANIEL FINULIAR", " "*2,"$")
 print("\t\t\t", "%"*37)
+
+print("\n\tARRAY VALUES: [81, 96, 77, 67, 43, 99, 65, 32, 62, 60]")
