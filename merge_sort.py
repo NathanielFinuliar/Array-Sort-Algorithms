c4e92727cf1a6c36a518360fb9457c453eaebf7a
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-05 16:44:12 +0800

    Prints Assigned Array Value to be merge sorted

--- merge_sort.py
+++ merge_sort.py
@@ -2,3 +2,5 @@ print("\t\t", "%"*37)
 print("\t\t $"," "* 9, "MERGE SORTING", " " *9,"$")
 print("\t\t $"," " * 2, "CODED BY NATHANIEL FINULIAR", " "*2,"$")
 print("\t\t", "%"*37)
+
+print("\n\tARRAY VALUES: [81, 96, 77, 67, 43, 99, 65, 32, 62, 60]")
