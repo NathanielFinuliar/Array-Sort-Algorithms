f5225aa1c5e473382b48388c6362a0bcc97af228
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-05 15:57:18 +0800

    Added ordinal_indicator for printing Numbered Passes

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,3 +4,13 @@ print("\t\t $"," " *2, "CODED BY NATHANIEL FINULIAR", " "*1,"$")
 print("\t\t", "%"*36)
 
 print("\n\tARRAY VALUES: [81, 96, 77, 67, 43, 99, 65, 32, 62, 60]")
+
+def ordinal_indicator(n):
+    if n == 1:
+        return "1st"
+    elif n == 2:
+        return "2nd"
+    elif n == 3:
+        return "3rd"
+    else:
+        return f"{n}th"
