13c5fd13a3bbd3683b5140d06609415eedc67305
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-05 17:02:46 +0800

    Prints Assigned Array Value to be quick sorted

--- quick_sort.py
+++ quick_sort.py
@@ -3,3 +3,5 @@ print("\t\t\t $"," "*9, "QUICK SORTING", " " *9,"$")
 print("\t\t\t $"," " *2, "CODED BY NATHANIEL FINULIAR", " "*2,"$")
 print("\t\t\t","%"*37)
 print(" ")
+
+print("\t  ARRAY VALUES:[81, 96, 77, 67, 43, 99, 65, 32, 62, 60]")
