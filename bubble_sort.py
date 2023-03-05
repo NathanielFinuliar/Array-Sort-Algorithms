9c1c5a0e3d2aa615dda4e3926158b47a82a40b30
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-05 15:55:59 +0800

    Prints Assigned Array Value to be bubble sorted

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,4 +1,6 @@
-print("\t\t\t", "%"*36)
-print("\t\t\t $"," "*8, "BUBBLE SORTING", " " *8,"$")
-print("\t\t\t $"," " *2, "CODED BY NATHANIEL FINULIAR", " "*1,"$")
-print("\t\t\t", "%"*36)
+print("\t\t", "%"*36)
+print("\t\t $"," "*8, "BUBBLE SORTING", " " *8,"$")
+print("\t\t $"," " *2, "CODED BY NATHANIEL FINULIAR", " "*1,"$")
+print("\t\t", "%"*36)
+
+print("\n\tARRAY VALUES: [81, 96, 77, 67, 43, 99, 65, 32, 62, 60]")
