27553358fb9469d141458d67d660bd121e1cd1b5
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-05 16:50:15 +0800

    Added merge_sort function and separation

--- merge_sort.py
+++ merge_sort.py
@@ -4,3 +4,16 @@ print("\t\t $"," " * 2, "CODED BY NATHANIEL FINULIAR", " "*2,"$")
 print("\t\t", "%"*37)
 
 print("\n\tARRAY VALUES: [81, 96, 77, 67, 43, 99, 65, 32, 62, 60]")
+
+def merge_sort(data): 
+    if len(data) == 1:                   
+        return data                      
+    mid = len(data) // 2                   
+    left = data[:mid]             
+    right = data[mid:]                   
+
+    left = merge_sort(left)              
+    right = merge_sort(right)              
+    data = merge(left, right)          
+    print("\t\t\t\t ", data)                        
+    return data                       
